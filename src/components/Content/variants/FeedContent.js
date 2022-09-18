30b340fe4c1965e20265976f133354d5e6f335f8
Artem Kotov <art.kotov1@gmail.com>
2022-09-18 11:32:08 +0300

    add gestures and sliders

--- src/components/Content/variants/FeedContent.js
+++ src/components/Content/variants/FeedContent.js
@@ -1,31 +1,71 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { createContext, useEffect, useMemo, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import styled from "styled-components";
-import image1 from './1.jpeg'
-import image2 from './2.jpeg'
+import axios from "axios";
+import FeedContentItem from "./FeedContentItem";
+import {useDrag, useMove} from "@use-gesture/react";
+import {useNavigate} from "react-router-dom";
+
+export const FeedPlayerContext = createContext({
+  isMuted: true,
+  setMutedState: () => {},
+});
+const FeedPlayerContextProvider = FeedPlayerContext.Provider;
 
 const FeedContent = () => {
-  return (
-      <StyledSwiper
-          direction="vertical"
-          autoHeight={true}
-          onSlideChange={() => console.log('slide change')}
-          onSwiper={(swiper) => console.log(swiper)}
+  const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  const [isMuted, setMutedState] = useState(true);
+  const feedPlayerContextValue = useMemo(
+    () => ({
+      isMuted,
+      setMutedState,
+    }),
+    [isMuted]
+  );
 
+  useEffect(() => {
+    const req = async () => {
+      const { data } = await axios.get("http://localhost:3005/feedItems?_page=1&_limit=3");
+      setData(data);
+    };
+    req();
+  }, []);
+
+  const lastSlideAction = async () => {
+      const { data } = await axios.get("http://localhost:3005/feedItems?_page=1&_limit=3");
+      setData(prevState => ([...prevState, ...data]));
+  }
+
+  const bind = useDrag((state) => {
+    if(state.elapsedTime < 200) {
+      if(state.direction[0] === 1 && state.distance[0] > 200) {
+        navigate(-1)
+      }
+      if(state.direction[0] === -1 && state.distance[0] > 200) {
+        navigate('/profile')
+      }
+    }
+  }, {axis: 'x'})
+
+  return (
+    <FeedPlayerContextProvider value={feedPlayerContextValue}>
+      {/*because slider fired end action with empty array*/}
+      <GestureElemet {...bind()}>
+      {data.length && <StyledSwiper
+        direction="vertical"
+        autoHeight={true}
+        onSlideChange={() => console.log("slide change")}
+        onReachEnd={lastSlideAction}
       >
-        <StyledSwiperSlide>
-          <img src={image2} alt=""/>
-        </StyledSwiperSlide>
-        <StyledSwiperSlide>
-          <img src={image1} alt=""/>
-        </StyledSwiperSlide>
-        <StyledSwiperSlide>
-          <img src={image2} alt=""/>
-        </StyledSwiperSlide>
-        <StyledSwiperSlide>
-          <img src={image1} alt=""/>
-        </StyledSwiperSlide>
-      </StyledSwiper>
+        {data.map((i, index) => (
+          <StyledSwiperSlide key={index}>
+            <FeedContentItem {...i} />
+          </StyledSwiperSlide>
+        ))}
+      </StyledSwiper>}
+      </GestureElemet>
+    </FeedPlayerContextProvider>
   );
 };
 
@@ -36,9 +76,14 @@ const StyledSwiper = styled(Swiper)`
   &.swiper-autoheight {
     height: 100%;
   }
+`;
+
+const GestureElemet = styled.div`
+height: 100%;
+  touch-action: none;
 `
 
 const StyledSwiperSlide = styled(SwiperSlide)`
   overflow: hidden;
   border-radius: 20px;
-`
+`;
